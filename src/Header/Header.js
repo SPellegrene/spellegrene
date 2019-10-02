c22f9ad80c74142275b16aa1095bf4d82eb1e498
Sam Pellegrene <pellegrenesam@gmail.com>
2019-10-02 07:38:28 -0600

    more

---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const header = () => {
+  return (
+      <nav id='wrapper'>
+        <li>INTRO</li>
+        <li>WHO</li>
+        <li>WHAT</li>
+        <li>WORK</li>
+      </nav>
+  )
+}
+
+export default header;
